7cda6aae36c6400df94a64651025e98d4c4f3240
drewparker <drewjparker91@gmail.com>
2020-08-24 13:12:35 -0700

    Add updateContact method
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>
    Co-authored-by: Garrett Brown. <garrettpaulbrown@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,48 @@ AddressBook.prototype.assignId = function() {
 
 AddressBook.prototype.findContact = function(id) {
   for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {     // <-- This line is new!
       if (this.contacts[i].id == id) {
         return this.contacts[i];
       }
-    };
+    }
+  };
   return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {     // <-- This line is new!
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+// AddressBook.prototype.updateContact = function(id) {
+//   contact.id = this.assignId();
+//   this.contacts.push(contact);
+// }
+
+AddressBook.prototype.updateContact = function(id, first, last, number) {
+  for (let i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        if (first != "") {
+          this.contacts[i].firstName = first;
+        }
+        if (last != "") {
+          this.contacts[i].lastName = last;
+        }
+        if (number != "") {
+          this.contacts[i].phoneNumber = number;
+        }
+      }
+    }
+  }
 }
 
 // Business Logic for Contacts ---------
@@ -33,3 +70,5 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+
